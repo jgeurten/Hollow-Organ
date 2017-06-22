ab3b86d99c5cbd47e0d81810590ea7c05feed677
Jordan Geurten <jordangeurten@gmail.com>
2017-06-22 15:03:59 -0400

    Correct encoder RPM

--- Firmware/DCMotorPID/DCMotorPID.ino
+++ Firmware/DCMotorPID/DCMotorPID.ino
@@ -4,19 +4,18 @@
 #define pi 3.1415
 
 volatile unsigned long edgeCount;
-volatile unsigned long motorW;
+volatile double motorW;
 
 void encoderISR()
 {
   edgeCount++;
-
 }
 
 ISR(TIMER1_OVF_vect) {
-  motorW = edgeCount / (2 * 16); //divided by 1 second [revolutions/minute]
+  motorW = (double) edgeCount*60 / (2 * 16 * 50); // multiply by the gear ratio (50:1) divided by 1 second [revolutions/minute]
   edgeCount = 0;
-  Serial.println(motorW); 
-  TCNT1=0x85EE; //restart timer with value of 34286 to give 1Hz
+
+  TCNT1 = 0x85EE; //restart timer with value of 34286 to give 1Hz
 }
 
 void setup()
@@ -28,24 +27,24 @@ void setup()
   digitalWrite(voltage, HIGH);
 
   cli();
-  TCCR1A = 0; 
+  TCCR1A = 0;
   TCCR1B = 0;
   TIMSK1 |= (1 << TOIE1);
-  TCNT1=0x85EE; //since using 8MHz clock, arduino example is doubled to give 1Hz
+  TCNT1 = 0x85EE; //since using 8MHz clock, arduino example is doubled to give 1Hz
   TCCR1B |= (1 << CS12);
   sei();
-  
+
   attachInterrupt(digitalPinToInterrupt(encoder), encoderISR, CHANGE);
-  analogWrite(motor, 200);
+  analogWrite(motor, 120);
 }
 
 void loop()
 {
-  /*for (int i = 100; i <= 255; i++) {
+  for (int i = 100; i < 256; i++)
+  {
+    Serial.println(motorW);
     analogWrite(motor, i);
-    Serial.print("Voltage:");
-    Serial.println(i);
-    delay(500);
-  }*/
+    delay(950);
+  }
 }
 
