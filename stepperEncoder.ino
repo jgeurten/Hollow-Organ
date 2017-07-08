9ac2b0cb8ffd89ad8de410dab4547768369c18fe
jgeurten <jjgeurte@edu.uwaterloo.ca>
2017-07-07 21:57:21 -0400

    Added speed calculation

--- stepperEncoder.ino
+++ stepperEncoder.ino
@@ -2,6 +2,7 @@ int inPinA = 0;
 int inPinB = 1; 
 
 volatile float encoderCount = 0; 
+volatile double motorSpeed; 
 
 void setup() {
   // put your setup code here, to run once:
@@ -9,6 +10,13 @@ void setup() {
   pinMode(inPinB, INPUT); 
 
   Serial.begin(9600); 
+
+  cli(); 
+  TCCR1A = 0; 
+  TCCR1B = 0; 
+  TCNT1 = 0x85EE; //since using 8MHz clock, arduino example is doubled to give 1Hz
+  TCCR1B |= (1 << CS12);
+  sei();
   
   attachInterrupt(digitalPinToInterrupt(inPinA), ISRPinA, CHANGE); 
   attachInterrupt(digitalPinToInterrupt(inPinB), ISRPinB, CHANGE); 
@@ -17,8 +25,8 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print("Encoder count:"); 
-  Serial.println(encoderCount); 
+  Serial.print("Speed:"); 
+  Serial.println(motorSpeed); 
 }
 
 void ISRPinA()
@@ -31,3 +39,9 @@ void ISRPinB()
   encoderCount++;
 }
 
+ISR(TIMER1_OVF_vect){
+  motorSpeed = (double)encoderCount*60/(2*16);  //   [revolutions/minute]
+  encoderCount = 0;
+
+  TCNT1 = 0x85EE; // 1 Hz (1 count per second)
+}
