6b4a06da2129ccb79dfa9e065ff39e3fb0aa9da8
Jordan Geurten <jordangeurten@gmail.com>
2017-07-10 14:03:55 -0400

    Added PID Code and Notes

--- Firmware/DCMotorPID/DCMotorPID.ino
+++ Firmware/DCMotorPID/DCMotorPID.ino
@@ -1,21 +1,79 @@
-#define encoder 2
-#define motor 5
-#define voltage 3
-#define pi 3.1415
+////////////////////////////////////////////////////////////
+// DC Motor PID Controller Code
+// Version 1, July 10
+// Jordan Geurten
+////////////////////////////////////////////////////////////
 
-volatile unsigned long edgeCount;
-volatile double motorW;
+#include <math.h>
 
+// Motor pins
+
+#define stopPin   0
+#define startPin  1
+#define encoder   2
+#define voltage   3
+#define motor     5
+
+//Desired speed:
+#define SPEED     30    //in RPM
+
+//Global variables:
+
+volatile byte Go = 0;                      //Interrupt flag for START
+volatile byte Stop = 0;                    //Interrup flag for STOP
+volatile double Error = 0;                 //Speed error
+volatile double KpOutput = 0;              //Proportional output
+volatile double KiOutput = 0;              //Integral output
+volatile double KdOutput = 0;              //Derivative output
+
+volatile unsigned long edgeCount = 0;      //Encoder edge count
+volatile double motorSpeed = 0;            //Motor speed in RPM
+
+//User defined variables:
+
+double Kp = 0;                             //Proportional gain
+double Ki = 0;                             //Integral gain
+double Kd = 0;                             //Derivative gain
+
+//ISR to increment count of encoder to count its square wave frequency
 void encoderISR()
 {
   edgeCount++;
 }
 
+//Every second, timer overflows and the speed of the motor is calculated. Encoder count is reset.
 ISR(TIMER1_OVF_vect) {
-  motorW = (double) edgeCount*60 / (2 * 16 * 50); // multiply by the gear ratio (50:1) divided by 1 second [revolutions/minute]
+  motorSpeed = (double) edgeCount * 60 / (2 * 16 * 50); // multiply by the gear ratio (50:1) divided by 1 second [revolutions/minute]
   edgeCount = 0;
 
   TCNT1 = 0x85EE; //restart timer with value of 34286 to give 1Hz
+}
+
+//Function to stop motor:
+void StopMotor()
+{
+  analogWrite(motor, 0);
+  digitalWrite(voltage, LOW);
+}
+
+//Interrupt function to start system:
+void START()
+{
+  Stop = 0; 
+  Go = 1; 
+}
+
+//Interrupt function to stop system:
+void STOP ()
+{
+  Stop = 1; 
+  Go = 0; 
+  StopMotor();
+}
+
+void calculateError()
+{
+    
 }
 
 void setup()
@@ -35,16 +93,16 @@ void setup()
   sei();
 
   attachInterrupt(digitalPinToInterrupt(encoder), encoderISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoder), encoderISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoder), encoderISR, CHANGE);
+
   analogWrite(motor, 120);
 }
 
 void loop()
 {
-  for (int i = 100; i < 256; i++)
-  {
-    Serial.println(motorW);
-    analogWrite(motor, i);
-    delay(950);
-  }
+
 }
+
+
 
